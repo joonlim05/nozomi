6852ec1f51f615ca99be737df48821626724646c
m3llo <choojoonlim@gmail.com>
2026-09-23 13:00:46 +0800

    add Seat entity and repository

---
+++ src/main/java/com/nozomi/models/Seat.java
@@ -0,0 +1,25 @@
+package com.nozomi.models;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+@Entity
+@Table (name = "seats")
+public class Seat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column (nullable = false)
+    private String seatNumber;
+
+    @Column (nullable = false)
+    private int carNumber;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "train_id", nullable = false)
+    private Train train;
+}
